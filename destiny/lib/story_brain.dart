b46395ad54402dd3d6469be7ffe7b85041c1ec4a
Dulio Denis <dulio.denis@yahoo.com>
2019-06-15 21:57:41 -0400

    Added buttonShouldBeVisible() to check if not story end node

--- destiny/lib/story_brain.dart
+++ destiny/lib/story_brain.dart
@@ -88,7 +88,6 @@ class StoryBrain {
         _storyNumber = 3;
       }
     }
-    print('New story is $_storyNumber');
 
     // Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
@@ -100,6 +99,13 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
-}
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+// Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    }
+    return false;
+  }
+}
