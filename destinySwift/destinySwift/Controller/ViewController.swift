b4712cdf9801c6eb73231e9def056f330ef49859
Dulio Denis <dulio.denis@yahoo.com>
2021-01-03 17:11:56 -0500

    Added Storyboard UI: background, buttons, AppIcon Set

--- destinySwift/destinySwift/Controller/ViewController.swift
+++ destinySwift/destinySwift/Controller/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
