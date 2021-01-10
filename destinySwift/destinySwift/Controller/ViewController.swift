b3b061af5758ae4f41992c8d8bcf46294ef6beac
Dulio Denis <dulio.denis@yahoo.com>
2021-01-10 17:39:02 -0500

    Added IBAction to link stories

--- destinySwift/destinySwift/Controller/ViewController.swift
+++ destinySwift/destinySwift/Controller/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = 0
+    
     let stories = [Story(t: "You see a fork in the road.", c1: "Take a left", c2: "Take a right"),
                       Story(t: "You see a tiger", c1: "Shout for help", c2: "Play dead"),
                       Story(t: "You find a treasure cheast.", c1: "Open it", c2: "Check for traps")
@@ -19,10 +21,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let opening = stories.first!
-        storyLabel.text = opening.title
-        choice1Button.setTitle(opening.choice1, for: .normal)
-        choice2Button.setTitle(opening.choice2, for: .normal)
+        updateUI()
+    }
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = String((sender.titleLabel?.text)!)
+        if (story < stories.count) {
+            if (choice == stories[story].choice1) { story += 1 }
+            else { story += 2 }
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        if (story < stories.count) {
+            let storyText = stories[story]
+            storyLabel.text = storyText.title
+            choice1Button.setTitle(storyText.choice1, for: .normal)
+            choice2Button.setTitle(storyText.choice2, for: .normal)
+        }
     }
 }
 
