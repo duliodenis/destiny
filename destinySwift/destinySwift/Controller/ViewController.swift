709f5173d019d7183849fdc89996b5c9414a8aa1
Dulio Denis <dulio.denis@yahoo.com>
2021-01-10 18:21:52 -0500

    Refactor by adding a StoryBrain struct to act as Model

--- destinySwift/destinySwift/Controller/ViewController.swift
+++ destinySwift/destinySwift/Controller/ViewController.swift
@@ -12,31 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    var story = 0
-    
-    let stories = [Story(t: "You see a fork in the road.", c1: "Take a left", c2: "Take a right"),
-                      Story(t: "You see a tiger", c1: "Shout for help", c2: "Play dead"),
-                      Story(t: "You find a treasure cheast.", c1: "Open it", c2: "Check for traps")
-    ]
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
-    
     @IBAction func choiceMade(_ sender: UIButton) {
-        let choice = String((sender.titleLabel?.text)!)
-        if (story < stories.count) {
-            if (choice == stories[story].choice1) { story += 1 }
-            else { story += 2 }
-            updateUI()
-        }
+        storyBrain.nextStory(userChoice: String((sender.titleLabel?.text)!))
+        updateUI()
     }
     
     func updateUI() {
-        if (story < stories.count) {
-            let storyText = stories[story]
+        if storyBrain.isMoreStories() {
+            let storyText = storyBrain.currentStory()
             storyLabel.text = storyText.title
             choice1Button.setTitle(storyText.choice1, for: .normal)
             choice2Button.setTitle(storyText.choice2, for: .normal)
--- destinySwift/destinySwift/Model/StoryBrain.swift
+++ destinySwift/destinySwift/Model/StoryBrain.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct StoryBrain {
+    var storyNumber = 0
+    
+    let stories = [Story(t: "You see a fork in the road.", c1: "Take a left", c2: "Take a right"),
+                   Story(t: "You see a tiger", c1: "Shout for help", c2: "Play dead"),
+                   Story(t: "You find a treasure cheast.", c1: "Open it", c2: "Check for traps")
+    ]
+    
+    mutating func nextStory(userChoice: String) {
+        if (storyNumber < stories.count) {
+            if (userChoice == stories[storyNumber].choice1) { storyNumber += 1 }
+            else { storyNumber += 2 }
+        }
+    }
+    
+    func isMoreStories() -> Bool {
+        if storyNumber < stories.count { return true}
+        return false
+    }
+    
+    func currentStory() -> Story {
+        return stories[storyNumber]
+    }
+}
