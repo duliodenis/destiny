51ce35f9a7d062e436c1aeebd081105d9fc7c164
Dulio Denis <dulio.denis@yahoo.com>
2021-01-10 17:17:07 -0500

    Refactored with a Story structure

--- destinySwift/destinySwift/Controller/ViewController.swift
+++ destinySwift/destinySwift/Controller/ViewController.swift
@@ -12,17 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left"
-    let choice2 = "Take a right"
+    let stories = [Story(t: "You see a fork in the road.", c1: "Take a left", c2: "Take a right"),
+                      Story(t: "You see a tiger", c1: "Shout for help", c2: "Play dead"),
+                      Story(t: "You find a treasure cheast.", c1: "Open it", c2: "Check for traps")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        let opening = stories.first!
+        storyLabel.text = opening.title
+        choice1Button.setTitle(opening.choice1, for: .normal)
+        choice2Button.setTitle(opening.choice2, for: .normal)
     }
-
-
 }
 
--- destinySwift/destinySwift/Model/Story.swift
+++ destinySwift/destinySwift/Model/Story.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    
+    init(t: String, c1: String, c2: String) {
+        title = t
+        choice1 = c1
+        choice2 = c2
+    }
+}
