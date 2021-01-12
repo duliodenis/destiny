c34a4562cbe34cc4af3271d52b41ee659abc47a9
Dulio Denis <dulio.denis@yahoo.com>
2021-01-11 19:44:54 -0500

    Refactor the StoryBrain for scalability

--- destinySwift/destinySwift/Model/Story.swift
+++ destinySwift/destinySwift/Model/Story.swift
@@ -10,11 +10,16 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
+    var choice1Destination: Int
     var choice2: String
+    var choice2Destination: Int
     
-    init(t: String, c1: String, c2: String) {
-        title = t
-        choice1 = c1
-        choice2 = c2
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
     }
 }
